5a682808cd6f70a2d14ea236308b2845d4ae9a80
Oshin5roroa <oshin@example.com>
2025-04-11 23:31:48 +0900

    Auto commit at 2025-04-11_23-31-48

--- app.py
+++ app.py
@@ -62,4 +62,5 @@ def upload_file(user_id):
     return render_template("upload.html", user_id=user_id)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
